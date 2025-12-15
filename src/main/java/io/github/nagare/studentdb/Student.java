3f972a773521304ef1923cba3465d6590d511dad
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-15 23:56:53 +1300

    student.java

---
+++ src/main/java/io/github/nagare/studentdb/Student.java
@@ -0,0 +1,74 @@
+package io.github.nagare.studentdb;
+
+import java.util.Objects;
+
+/**
+ * Represents a student record.
+ */
+public class Student {
+    private String id;
+    private String name;
+    private String firstName;
+    private Degree degree;
+
+    public Student() {
+    }
+
+    public Student(String id, String firstName, String name, Degree degree) {
+        this.id = id;
+        this.firstName = firstName;
+        this.name = name;
+        this.degree = degree;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Degree getDegree() {
+        return degree;
+    }
+
+    public void setDegree(Degree degree) {
+        this.degree = degree;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{id='" + id + "', firstName='" + firstName +
+                "', name='" + name + "', degree=" + degree + "}";
+    }
+}
